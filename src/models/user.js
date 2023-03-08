6c2ccb0b811218389fad439332b8c7f0aea07788
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-03-09 01:18:48 +0530

    User Model Update: store encrypted password and generate JWT

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -15,6 +17,29 @@ const userSchema = new mongoose.Schema({
         require: true,
     }
 });
+
+
+// storing the plain password in the encrypted form
+userSchema.pre('save', function(next) {
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const hash = bcrypt.hashSync(user.password, SALT);
+    user.password = hash;
+    next();
+});
+
+
+// compare the plain password with the encrypted password
+userSchema.methods.comparePassword = function compare(password) {
+    const response = bcrypt.compareSync(password, this.password);
+    return response;
+}
+
+
+userSchema.methods.generateJWT = function generate() {
+    const token = jwt.sign({id: this._id, email: this.email}, process.env.JWT_KEY, {expiresIn: '1d'});
+    return token;
+}
 
 const User = mongoose.model('User', userSchema);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../../controller/user-controller.js';
+import { signup, signin } from '../../controller/user-controller.js';
 
 const router = express.Router();
 
 router.post('/create', signup);
+router.post('/signin', signin);
 
 export default router;
